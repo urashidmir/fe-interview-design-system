a11020d294bf09347be810f30585ff8be5c5aef9
urashidmir <umer.rashid.mir@gmail.com>
2024-09-24 17:42:32 +0100

    Types added

---
+++ src/types/tabs.ts
@@ -0,0 +1,29 @@
+
+export enum BadgeVariant {
+    Positive = 'positive',
+    Negative = 'negative',
+    Neutral = 'neutral',
+}
+  
+export type BadgeItem = {
+    label: string;
+    variant: BadgeVariant; 
+};
+
+export type TabItem = {
+    id: number;
+    label: string;
+    icon? : boolean;
+    badge? : BadgeItem | null;
+    timer? : boolean;
+};
+
+export enum TabVariant {
+    Pill = 'pill',
+    Underline = 'underline',
+  }
+  
+export type TabSet = {
+    tabs: TabItem[];      
+    variant: TabVariant;  
+};
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Automatically import the variables file in every SCSS file
+        additionalData: `@import "./src/styles/_variables.scss";`,
+      },
+    },
+  },
   test: {
     include: ["src/**/*.test.{js,ts,tsx}"],
     globals: true, //https://vitest.dev/guide/migration.html#globals-as-a-default
